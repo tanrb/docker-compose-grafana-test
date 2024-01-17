852c2bae04e6aeb4d2a0b968ff4b17126a9529f2
Ryan Lim <e0772466@u.nus.edu>
2024-01-17 14:59:15 +0800

    Added MongoDB container and connected endpoints from services A and B

--- service_a/main.py
+++ service_a/main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
+import requests
 
 app = FastAPI()
 
 ## A will get update
 ## B will validate the update and update the db and return 200 ok 
+
+@app.get("/hello")
+async def hello():
+    # use internal docker network to communicate with service_b (port 80 exposed)
+    response = requests.get("http://service_b:80/")
+    return response.json()
 
 @app.get("/")
 async def create():
