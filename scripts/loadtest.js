d5d6bf92d1d018fa030a623e675fe27b0465dfdd
tanrb <rbtan1999@gmail.com>
2024-01-22 11:16:36 +0800

    latest commit for gitlab

--- scripts/loadtest.js
+++ scripts/loadtest.js
@@ -7,6 +7,9 @@ export let options = {
         { duration: '20s', target: 1 }, // Stay at 100 users for 10 minutes
         { duration: '10s', target: 0 }, // Ramp down to 0 users over 5 minutes
     ],
+    thresholds: {
+        http_req_duration: ['p(99)<10']
+    }
 };
 
 export default function () {
