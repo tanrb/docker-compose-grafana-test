4dbc08e00adff8c2d4fd3906fbb4e71ed42fa560
tanrb <rbtan1999@gmail.com>
2024-01-25 18:25:37 +0800

    erorr

--- scripts/loadtest.js
+++ scripts/loadtest.js
@@ -3,8 +3,8 @@ import { check, sleep } from 'k6';
 
 export let options = {
     stages: [
-        { duration: '10s', target: 1 }, // Ramp up to 100 users over 5 minutes
-        { duration: '20s', target: 1 }, // Stay at 100 users for 10 minutes
+        { duration: '10s', target: 100 }, // Ramp up to 100 users over 5 minutes
+        { duration: '20s', target: 100 }, // Stay at 100 users for 10 minutes
         { duration: '10s', target: 0 }, // Ramp down to 0 users over 5 minutes
     ],
     thresholds: {
