08f37ad51eecec616fbe4110c827ae4db8b841cb
Ryan Lim <e0772466@u.nus.edu>
2024-01-31 13:55:20 +0800

    automatic instrumentation working

--- auth_service/main.py
+++ auth_service/main.py
@@ -15,36 +15,36 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor
 import logging
 
 
-# Set up logger
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-ch = logging.StreamHandler()
-format_str = '%(levelname)s:%(lineno)s:%(message)s'
-formatter = logging.Formatter(format_str)
-ch.setFormatter(formatter)
-logger.addHandler(ch)
-logger.info('Logger ready')
+# # Set up logger
+# logger = logging.getLogger(__name__)
+# logger.setLevel(logging.DEBUG)
+# ch = logging.StreamHandler()
+# format_str = '%(levelname)s:%(lineno)s:%(message)s'
+# formatter = logging.Formatter(format_str)
+# ch.setFormatter(formatter)
+# logger.addHandler(ch)
+# logger.info('Logger ready')
 
 
-OTEL_EXPORTER_OTLP_ENDPOINT="http://otelcol:4137/v1/traces"
-OTEL_RESOURCE_ATTRIBUTES="service.name=auth-service"
+# OTEL_EXPORTER_OTLP_ENDPOINT="http://otelcol:4137/v1/traces"
+# OTEL_RESOURCE_ATTRIBUTES="service.name=auth-service"
 
 
-# Sets the global default tracer provider
+# # Sets the global default tracer provider
 
 
-# Creates a tracer from the global tracer provider
-tracer = trace.get_tracer("my.tracer.name")
+# # Creates a tracer from the global tracer provider
+# tracer = trace.get_tracer("my.tracer.name")
 
 
-resource = Resource(attributes={
-    SERVICE_NAME: OTEL_RESOURCE_ATTRIBUTES
-})
+# resource = Resource(attributes={
+#     SERVICE_NAME: OTEL_RESOURCE_ATTRIBUTES
+# })
 
-traceProvider = TracerProvider(resource=resource)
-processor = BatchSpanProcessor(OTLPSpanExporter(endpoint=OTEL_EXPORTER_OTLP_ENDPOINT))
-traceProvider.add_span_processor(processor)
-trace.set_tracer_provider(traceProvider)
+# traceProvider = TracerProvider(resource=resource)
+# processor = BatchSpanProcessor(OTLPSpanExporter(endpoint=OTEL_EXPORTER_OTLP_ENDPOINT))
+# traceProvider.add_span_processor(processor)
+# trace.set_tracer_provider(traceProvider)
 
 app = FastAPI()
 Instrumentator().instrument(app).expose(app)
@@ -60,7 +60,7 @@ async def hello():
 
 @app.post("/authenticate")
 async def auth(request: Request):
-    with tracer.start_as_current_span("auth-user") as span:
+    # with tracer.start_as_current_span("auth-user") as span:
         # Get username from request
         body = await request.json()
         username = body["username"]
@@ -68,7 +68,7 @@ async def auth(request: Request):
         # Get hash from database
         user = db.test.find_one({"username": username})
         current_span = trace.get_current_span()
-        logger.info(current_span)
+        # logger.info(current_span)
         
         # If user does not exist, just return failures
         if (user == None):
@@ -79,18 +79,18 @@ async def auth(request: Request):
         # Calculate hash from whatever the user entered
         hash = body["hash"]
         
-        logger.info(current_span)
+        # logger.info(current_span)
         # Compare the two hashes
         if hash == db_hash:
             #return 200 OK
             return {"message": "Success"}
         else:
             #return 401 Unauthorized
-            span.add_event("log", {
-                "log.severity": "error",
-                "log.message": "User not found",
-                "enduser.id": username,
-            })
+            # span.add_event("log", {
+            #     "log.severity": "error",
+            #     "log.message": "User not found",
+            #     "enduser.id": username,
+            # })
             return {"message": "Failure"}
         
 
